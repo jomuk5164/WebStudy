09aca7570f3305e035000ea127c6ca7ffd031590
KimJongUk <jomuk5164@gmail.com>
2022-08-09 21:39:47 +0900

    js study

---
+++ js/test.js
@@ -0,0 +1,15 @@
+'use stric'; //자동 형 변환 방지
+
+// undefined = 변수 선언은 했지만 값이 없음
+// null = 값이 없음
+// NaN = 숫자가 아님
+
+//자료형 - 문자열 / 숫자열 / 
+//배열 [index] = 데이터들을 그룹화 해서 관리 , 속성 : length(길이) , push(배열끝에 값 추가) ex) a.length , a.push
+// 비교연산자, 논리연산자 OR(||) AND(&&) NOT(!) 
+
+
+
+let movies = ['어벤져스','오펀','강시','나홀로집에','내부자들'] ;
+
+console.log(movies[5])
