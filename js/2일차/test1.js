30cf40565518b4ef05dd42b6ae29263f652c83df
KimJongUk <jomuk5164@gmail.com>
2022-08-10 19:37:28 +0900

    test1

---
+++ js/2일차/test1.js
@@ -0,0 +1,16 @@
+'use stric';
+/*
+let person = prompt("몇명이 입장 하시나요?") ;
+let line = prompt("한줄에 몇명씩 앉나요?") ;
+
+
+ if (person%line === 0) {
+    
+    alert("필요한 줄은"+ parseInt(person/line) +"줄 입니다.") ;
+}
+else {
+    
+    alert("필요한 줄은" + (parseInt(person/line) + 1) + "줄 입니다.") ;
+} */
+
+console.log (parseInt (48/7)) ;
