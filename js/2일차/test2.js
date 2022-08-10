574ae605fbc0218d7d9186ef20bcc7c239437cb5
KimJongUk <jomuk5164@gmail.com>
2022-08-10 19:53:49 +0900

    test2

---
+++ js/2일차/test2.js
@@ -0,0 +1,14 @@
+'use stric'
+
+
+
+for (let num = 2; num < 10; num++){
+
+    document.write(num + "단</br>") ;
+for (i=1; i<10; i++) {
+    let sum = num*i ;
+    document.write(num + " X " + i + " = " + sum + "</br>" ) ;
+    
+}
+
+}
